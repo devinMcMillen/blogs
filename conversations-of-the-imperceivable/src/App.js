e6dcb4d976ada872c43f1de9e7d0e39e743dfa9e
Devin McMillen <dmcmille90@protonmail.com>
2022-12-20 08:40:39 -0800

    Bring in React Router. Add images for the site. Create Navbar, AboutPage, HomePage, NotFoundPage for base content.

--- conversations-of-the-imperceivable/src/App.js
+++ conversations-of-the-imperceivable/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import 'bulma/css/bulma.min.css';
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './components/layout/Navbar.js';
+import HomePage from "./pages/HomePage.js";
+import PageNotFound from "./pages/NotFoundPage.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="main">
+      <NavBar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route index element={<HomePage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ conversations-of-the-imperceivable/src/pages/AboutPage.js
@@ -0,0 +1,8 @@
+// My name is Ethan E. Real -> Eth-E-Real -> ethereal
+function AboutPage() {
+    return (
+        <div>About Page</div>
+    );
+}
+
+export default AboutPage;
